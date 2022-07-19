e8bf9a44dbdccdc6b9baec1e267f418dda79b567
Shatadru Datta <shatadru.datta.contractor@pepsico.com>
2022-07-20 01:11:56 +0530

    commit

--- Sources/pep-swift-shngen-charts/pep_swift_shngen_charts.swift
+++ Sources/pep-swift-shngen-charts/pep_swift_shngen_charts.swift
@@ -1,6 +1,20 @@
-public struct pep_swift_shngen_charts {
+import SwiftUI
+
+@available(macOS 10.15.0, *)
+public struct BarChartView: View {
     public private(set) var text = "Hello, World!"
 
-    public init() {
+    public init() { }
+    
+    @available(iOS 13.0.0, *)
+    public var body: some View {
+        VStack(alignment: .leading) {
+            HStack(alignment: .top) {
+                Text("Hello")
+                Text("Shatadru")
+                Text("How are you !!!")
+            }
+        }
     }
 }
+
